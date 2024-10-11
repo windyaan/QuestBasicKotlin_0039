25fc39cf5dbb1aeb707b3e976b34ba29351b3f0f
windyaan <windyanurmaretha@gmail.com>
2024-10-11 15:54:28 +0700

    Mengubah output pada objek kontak menjadi jane. val membuat nilai pada objek tidak berubah

--- app/src/main/java/com/example/prakmobile1/Class.kt
+++ app/src/main/java/com/example/prakmobile1/Class.kt
@@ -3,3 +3,18 @@ package com.example.prakmobile1
 class Motor()
 
 class Contact (val id: Int, var email: String)
+
+fun main() {
+
+    val contact = Contact(1, "mary@gmail.com")
+
+// Prints the value of the property: email
+    println(contact.email)
+
+// Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+// Prints the new value of the property: email
+    println(contact.email)
+
+}

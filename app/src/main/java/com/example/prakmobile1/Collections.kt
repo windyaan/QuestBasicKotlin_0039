bd6b84050cb4032f1fc29d670e172f727b0f2f60
windyaan <windyanurmaretha@gmail.com>
2024-10-11 16:50:52 +0700

    Contoh Map dan mengubah data dlm Map

--- app/src/main/java/com/example/prakmobile1/Collections.kt
+++ app/src/main/java/com/example/prakmobile1/Collections.kt
@@ -69,3 +69,31 @@ fun ContohSet() {
 // Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
 // Map Read-Only menggunakan mapOf
 // Map Mutable menggunakan mutableMapOf
+
+fun ContohMap() {
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dlm Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dlm Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}

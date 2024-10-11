608439f8653d4eb3bf0f0e94d496bfdfc45d2377
windyaan <windyanurmaretha@gmail.com>
2024-10-11 16:02:01 +0700

    penjelasan list

---
+++ app/src/main/java/com/example/prakmobile1/Collections.kt
@@ -0,0 +1,8 @@
+package com.example.prakmobile1
+
+// List adalah Kumpulan data yang disusun secara berurutan dari
+// List bersifat ordered, artinya data yang dimasukkan akan meniliki posisi sesuai urutan
+// List bersifat mutable dan read only, artinya data yang masukkan dapat diubah dan tidak dapat diubah
+// List Read-Only menggunakan ListOf
+// List Mutable menggunakan mutableListOf
+

3bad5c503f0d41c174b74fb5c7d0044cafcf1642
windyaan <windyanurmaretha@gmail.com>
2024-10-11 16:47:40 +0700

    Contoh List dan mengubah data dlm List

--- app/src/main/java/com/example/prakmobile1/Collections.kt
+++ app/src/main/java/com/example/prakmobile1/Collections.kt
@@ -6,3 +6,29 @@ package com.example.prakmobile1
 // List Read-Only menggunakan ListOf
 // List Mutable menggunakan mutableListOf
 
+fun ContohList() {
+    println("=== List ===")
+    // List Read-Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Mengembalikan data ke dalan List Mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}

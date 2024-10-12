d1ebe88914d30a8226bc122350eb10459774a5fa
windyaan <windyanurmaretha@gmail.com>
2024-10-12 14:23:51 +0700

    contoh penggunaan parameter dalam fungsi

---
+++ app/src/main/java/com/example/prakmobile1/Functions.kt
@@ -0,0 +1,12 @@
+package com.example.prakmobile1
+
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}

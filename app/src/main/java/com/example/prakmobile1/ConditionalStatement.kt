1112bd5ebc109f73ddad3bbe00a8560ee8b1cf90
windyaan <windyanurmaretha@gmail.com>
2024-10-12 14:04:37 +0700

    pengertian conditional expression

--- app/src/main/java/com/example/prakmobile1/ConditionalStatement.kt
+++ app/src/main/java/com/example/prakmobile1/ConditionalStatement.kt
@@ -1,2 +1,5 @@
 package com.example.prakmobile1
 
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch

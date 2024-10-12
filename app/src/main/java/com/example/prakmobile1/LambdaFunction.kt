c5a9c97df8bb60921eb1f4499d45e48eba68310b
windyaan <windyanurmaretha@gmail.com>
2024-10-12 14:34:02 +0700

    pengertian lambda functions

---
+++ app/src/main/java/com/example/prakmobile1/LambdaFunction.kt
@@ -0,0 +1,6 @@
+package com.example.prakmobile1
+
+// Lambda Functions
+// Lambda functions adalah fungsi yang tidak memiliki nama
+// Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi

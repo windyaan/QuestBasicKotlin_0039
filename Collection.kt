130e858ee7b71e915359e4e63c2a9fb5b5e1f4fc
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-24 17:56:46 +0700

    keterangan data set

--- Collection.kt
+++ Collection.kt
@@ -32,3 +32,9 @@ fun ContohList() {
     val shapesLocked: List<String> = shape
     println(shapesLocked)
 }
+
+// Set adalah Kumpulan data yang tidak memiliki posisi atau urutan tertentu dan hanya menyimpan data unik(tidak ada duplikasi)
+// Set bersifat unordered, artinya data yang dimasukkan tidak memiliki posisi tertentu
+// set bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Set Read-Only menggunakan setOf
+// Set Mutable senggunakan MutableSetOf

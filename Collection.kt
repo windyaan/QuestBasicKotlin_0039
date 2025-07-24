25351e682b584d2b8a96844a315ad6cdf291f06d
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-24 17:59:16 +0700

    keterangan data map

--- Collection.kt
+++ Collection.kt
@@ -66,3 +66,6 @@ fun ContohSet() {
 
 // Map adalah Kumpulan data yang disusun dalam pasangan key-value
 // Map bersifat unordered, artinya data yang dimasukkan tidak nemiliki posisi tertentu
+// Map bersifat mutable dan read only, artinya data yang dimasukkan dapat diubah dan tidak dapat diubah
+// Map Read-Only menggunakan mapOf
+// Map Mutable menggunakan mutableMapOf

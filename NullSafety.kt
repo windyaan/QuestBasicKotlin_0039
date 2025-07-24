171692e7c8833d1e66af6a37e362094f57ce5396
Windya Nurmaretha <windyanurmaretha@gmail.com>
2025-07-24 18:22:53 +0700

    setting data null

---
+++ NullSafety.kt
@@ -0,0 +1,21 @@
+package com.example.test
+
+fun nullSafety() {
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    // Throws a compiler error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here"
+    // This is OK
+    nullable = null
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+}
